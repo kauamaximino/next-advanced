79efa8dcfffe831fdee36e5e9e5b1c25477e1c80
Kauã Maximino <kaua_maximino@outlook.com>
2022-11-18 20:05:18 -0300

    feat: :sparkles: client and serven in NextJS

--- my-project-nextjs/pages/about/index.tsx
+++ my-project-nextjs/pages/about/index.tsx
@@ -1,10 +1,20 @@
+import Link from 'next/link';
+import { useState } from 'react';
+
 const About = () => {
+  const [count, setCount] = useState(0);
+
   return (
     <div>
       <h1>Page About</h1>
+      <p>Count: {count}</p>
 
-      <a href="/about/kaua">Kauã</a>
-      <a href="/about/maximino">Maximino</a>
+      <Link href="/about/kaua">Kauã</Link>
+      <Link href="/about/maximino">Maximino</Link>
+
+      <button onClick={() => {setCount(count + 1)}}>Aumentar count</button>
+      <button onClick={() => {setCount(count - 1)}}>Diminuir count</button>
+      <button onClick={() => {setCount(0)}}>Zerar count</button>
     </div>
   );
 };
