b9d1bcc4a006135f518e8b9c2bed76249772dc03
Kauã Maximino <maximinokaua@gmail.com>
2022-12-02 08:50:35 -0300

    build: :wrench: next.config

--- my-project-nextjs/next.config.js
+++ my-project-nextjs/next.config.js
@@ -1,7 +1,15 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
+module.exports = {
+  async headers() {
+    return [
+      {
+        source: '/api/:path*',
+        headers: [
+          {
+            key: 'Access-Control-Allow-Origin',
+            value: '*',
+          }
+        ],
+      },
+    ]
+  },
 }
-
-module.exports = nextConfig
