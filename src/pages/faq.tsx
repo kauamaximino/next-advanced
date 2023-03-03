deb577c9bf3a8594da2a2c9c44f0905e6ef44036
Kauã Maximino <maximinokaua@gmail.com>
2023-03-03 08:52:25 -0300

    faq page

--- src/pages/faq.tsx
+++ src/pages/faq.tsx
@@ -1,15 +1,24 @@
-import Head  from 'next/head';
-import Link from '../components/Link';
-import HeadGeneral from '@/components/Head';
+// faq.js
+import FAQScreen from '../screens/FAQScreen';
 
-function FAQPage() {
-  return ( 
-    <div>
-      <HeadGeneral title="FAQ" />
-      <h1>FAQ</h1>
-      <Link href="/">Ir para a HomePage</Link>
-    </div>  
-  )
-}
+export default FAQScreen; 
 
-export default FAQPage;
+export async function getStaticProps() {
+    console.log('Em modo DEV, sempre roda! A cada acesso')
+    console.log('Roda SOMENTE em build time')
+    const FAQ_API_URL = 'https://gist.githubusercontent.com/omariosouto/0ceab54bdd8182cbd1a4549d32945c1a/raw/578ad1e8e5296fa048e3e7ff6b317f7497b31ad9/alura-cases-faq.json';
+    const faq = await fetch(FAQ_API_URL)
+        .then((respostaDoServidor) => {
+            return respostaDoServidor.json();
+        })
+        .then((resposta) => {
+            return resposta;
+        });
+
+    return {
+      props: {
+          qualquercoisa: 'que eu passar aqui',
+          faq,
+      },
+    };
+}  
